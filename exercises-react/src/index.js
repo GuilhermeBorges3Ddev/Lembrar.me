87b57fb8dac7021fbd13d3a61db05d9660b62ee6
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-07-03 22:55:15 -0300

    index.js refactored

--- exercises-react/src/index.js
+++ exercises-react/src/index.js
@@ -5,12 +5,15 @@ const element = document.getElementById("root");
 const jsx = <h1>Hello React!</h1>;
 ReactDOM.render(jsx, element);
 
-// THESE CODE RUN WELL!!!
-// const element = document.getElementById("root");
-// ReactDOM.render(
-//   <ul>
-//     <li>Are you a HTML converted in JS?</li>
-//     <li>Yes, I'm too!</li>
-//   </ul>,
-//   element
-// );
+/* 
+THESE CODE RUNS WELL! TOO!!
+
+const element = document.getElementById("root");
+
+ReactDOM.render(
+   <ul>
+     <li>Are you a HTML converted in JS?</li>
+     <li>Yes, I'm too!</li>
+   </ul>,
+element);
+*/
